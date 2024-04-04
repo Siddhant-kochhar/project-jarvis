ae6badc5701f3fdf7e0e5aaa22688d5dddb30957
deepak_kochhar <deepak.kochhar@magicpin.in>
2024-04-04 23:24:45 +0530

    project updates

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 https://skills.ai/blog/import-google-sheets-to-pandas/
 
 
-
 - suggestions
 -- store student's subject wise proficiency
 
 
-- test
+- steps to run the project
+make run
+open index.html in chrome
+enter student_id, subject and your query (example:)
+
--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,4 @@
 GOOGLE_FORM = "https://forms.gle/CYd6GThxfi1arymS9"
 GFORM_RESPONSES = "https://docs.google.com/spreadsheets/d/11DvmwhmV1FsKfPkHqa7M6VXWolp6tX39FdpbikSPinY/edit?resourcekey#gid=1595136244"
 GFORM_RESPONSES_EXPORT = "https://docs.google.com/spreadsheets/d/11DvmwhmV1FsKfPkHqa7M6VXWolp6tX39FdpbikSPinY/export?format=csv&gid=1595136244"
+OPEN_AI_KEY = ""
