d1e9816c54ea1350d692286f4fe285f18edebba8
Siddhant Kochhar <siddhant@macbook-pro-sk.local>
2024-04-04 19:22:06 +0530

    changes

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,3 @@
 GOOGLE_FORM = "https://forms.gle/CYd6GThxfi1arymS9"
 GFORM_RESPONSES = "https://docs.google.com/spreadsheets/d/11DvmwhmV1FsKfPkHqa7M6VXWolp6tX39FdpbikSPinY/edit?resourcekey#gid=1595136244"
 GFORM_RESPONSES_EXPORT = "https://docs.google.com/spreadsheets/d/11DvmwhmV1FsKfPkHqa7M6VXWolp6tX39FdpbikSPinY/export?format=csv&gid=1595136244"
-OPEN_AI_KEY = "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"
